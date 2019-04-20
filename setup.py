418805900d7072a09e8ce4cbf32f448cdefb83c6
Nate <nharmon8@gmail.com>
2019-04-20 09:49:26 -0600

    Packaging

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+
+setup(
+    name='DSD',
+    version='1.0',
+    packages=['dsd',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description=open('README.md').read(),
+    install_requires=[
+        "numpy >= 1.14.4",
+        "pandas >= 0.24.2",
+        "urllib3 >= 1.13.1",
+    ],
+)
